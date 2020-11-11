7cdd92849fd2226d1adcadc6730d49f89f3581d7
JeanChristophe Sabatier <sabatier.jeanchristophe@gmail.com>
2020-11-11 18:15:51 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # for listing all retaurants
+  # get "restaurants", to: "restaurants#index", as: :restaurants
+  # for showing a specific restaurant/id:3
+  # get "restaurants/:id", to:"restaurants#show", as: :restaurant
+  # for restaurant creation by user
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants/create",
+
+  # update a specific restaurant/id:3
+  #patch "restaurants/:id", to:"restaurants#update", as: :edit_restaurant
+
+  # for adding a review
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+  # get "/restaurants/:restaurant_id/review/new", to:"reviews/new", as: :new_restaurant_review
+  # post "/restaurants/:restaurant_id/review", to:"reviews/create", as: :restaurant_reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east =  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+wayo_wayo = { name: 'Wayo Wayo', address: 'Les Halles Bacalan, Bordeaux', category: 'french' }
+frida = { name: 'Frida', address: 'Rue de la grosse cloche, Bordeaux', category: 'belgian' }
+hutong = { name: 'Hutong', address: 'rue du tondu, Bordeaux', category: 'japanese' }
+
+[dishoom, pizza_east, wayo_wayo, frida, hutong].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
